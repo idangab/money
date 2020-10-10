bf990683f90cfee05a0ebb3ba3709875237bc247
SaharH2 <72566208+SaharH2@users.noreply.github.com>
2020-10-10 13:14:04 +0300

    Add files via upload

---
+++ strategy1.py
@@ -0,0 +1,19 @@
+def perform_strategy(envelope, counter):
+    for i in range(len(envelope)):
+        if i == 69:
+            envelope[i].used =True
+            print( "The money from the envelope: " + str(self.envelope[i].money))
+        else:
+            envelope[i].used = True
+
+
+\
+
+
+
+
+
+
+
+
+
